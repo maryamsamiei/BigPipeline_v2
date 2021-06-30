3b2ec616454ee79491b3cafe4642313493641c45
Maryam SN <69000702+maryamsamiei@users.noreply.github.com>
2021-06-30 11:50:27 -0500

    Update EAML_Functions.py
    adjusting for VEP annotation

--- src/EAML_Functions.py
+++ src/EAML_Functions.py
@@ -20,12 +20,13 @@ from pkg_resources import resource_filename
 from scipy import stats
 from statsmodels.stats.multitest import multipletests
 from tqdm import tqdm
-from vcf import parse_gene_eaml
+
 from visualize import mcc_hist, mcc_scatter, manhattan_plot
 from weka_wrapper import eval_gene1
+from vcf import *
 
 
-def compute_gene_dmatrix(gene,reference, data_fn, targets_fn, write_data, min_af, max_af, af_field, EA_parser, expdir):
+def compute_gene_dmatrix(gene,reference, data_fn, targets_fn, min_af, max_af, af_field, EA_parser, EA_Ann, expdir):
     """
     Computes the full design matrix from an input VCF
 
@@ -36,25 +37,29 @@ def compute_gene_dmatrix(gene,reference, data_fn, targets_fn, write_data, min_af
         DataFrame: EA design matrix for gene-of-interest
     """
     targets = pd.read_csv(targets_fn, header=None, dtype={0: str, 1: int}).set_index(0).squeeze().sort_index()
-    if reference=='hg19':
-        ref = pd.read_csv('./refs/hg19-refGene.protein-coding.txt', delimiter='\t', header=0, index_col='name2')
-    elif reference=='hg38':
-        ref = pd.read_csv('./refs/hg38-refGene.protein-coding.txt', delimiter='\t', header=0, index_col='name2')
+    if EA_Ann=='ANNOVAR':
+        if reference=='hg19':
+            ref = pd.read_csv('./refs/refGene-lite_hg19.May2013.txt', delimiter='\t', header=0, index_col='name2')
+        elif reference=='hg38':
+            ref = pd.read_csv('./refs/refGene-lite_hg38.June2017.txt', delimiter='\t', header=0, index_col='name2')
+    elif EA_Ann=='VEP':
+        if reference=='hg19':
+            ref = pd.read_csv('./refs/ENSEMBL-lite_GRCh37.v75.txt', delimiter='\t', header=0, index_col='gene')
+        elif reference=='hg38':
+            ref = pd.read_csv('./refs/ENSEMBL-lite_GRCh38.v94.txt', delimiter='\t', header=0, index_col='gene')
+        ref = ref[~ref.index.duplicated(keep='first')] 
     gene_reference = ref.loc[gene]
-    dmatrix = parse_gene_eaml(data_fn, gene, gene_reference, list(targets.index), min_af,max_af, af_field, EA_parser)
-    # if write_data==1:
-    #     written = False
-    #     while written is False:
-    #         try:
-    #             dmatrix.to_hdf(expdir+'dmatrices.h5', key=gene, complevel=5, complib='zlib', format='fixed')
-    #             written = True
-    #         except HDF5ExtError:
-    #             pass
+    if EA_Ann=='ANNOVAR':
+        dmatrix = parse_ANNOVAR_eaml(data_fn, gene, gene_reference, list(targets.index), min_af,max_af, af_field, EA_parser)
+    elif EA_Ann=='VEP':
+        dmatrix = parse_VEP_eaml(data_fn, gene, gene_reference, list(targets.index), min_af=min_af,max_af=max_af, af_field=af_field, EA_parser=EA_parser)
+
+    
     return dmatrix
 
 
 
-def eval_gene(gene,reference,data_fn,targets_fn,expdir,write_data, min_af, max_af, af_field, EA_parser,seed=111,cv=10,weka_path='./weka-3-8-5/'):
+def eval_gene(gene,reference,data_fn,targets_fn,expdir, min_af, max_af, af_field, EA_parser,EA_Ann, seed=111,cv=10,weka_path='./weka-3-8-5/'):
     """
     Parses input data for a given gene and evaluates it using Weka
 
@@ -76,7 +81,7 @@ def eval_gene(gene,reference,data_fn,targets_fn,expdir,write_data, min_af, max_a
         'MultilayerPerceptron': '-L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a'
     }
     targets = pd.read_csv(targets_fn, header=None, dtype={0: str, 1: int}).set_index(0).squeeze().sort_index()
-    gene_dmatrix = compute_gene_dmatrix(gene,reference, data_fn,targets_fn, write_data, min_af, max_af, af_field, EA_parser,expdir)
+    gene_dmatrix = compute_gene_dmatrix(gene,reference, data_fn,targets_fn, min_af, max_af, af_field, EA_parser, EA_Ann, expdir)
     mcc_results = eval_gene1(gene, gene_dmatrix, targets, class_params, 111, 10, Path(expdir), weka_path='./weka-3-8-5/')
     (Path(expdir) / f'tmp/{gene}.arff').unlink()  # clear intermediate ARFF file after gene scoring completes
     return gene, mcc_results
@@ -111,17 +116,14 @@ def report_results(raw_results,expdir):
     return full_results,nonzero_results
 
 
-def visualize(full_results, nonzero_results, expdir, reference):
-    """
-    Generate summary figures of EA-ML results, including a Manhattan plot of p-values and scatterplots and
-    histograms of MCC scores
-    """
-    mcc_scatter(full_results, column='mean', dpi=150).savefig(expdir + f'meanMCC-scatter.png')
-    mcc_hist(full_results, column='mean', dpi=150).savefig(expdir + f'meanMCC-hist.png')
-    mcc_scatter(nonzero_results, column='MCC', dpi=150).savefig(expdir + 'meanMCC-scatter.nonzero.png')
-    mcc_hist(nonzero_results, column='MCC', dpi=150).savefig(expdir + 'meanMCC-hist.nonzero.png')
-    manhattan_plot(nonzero_results, reference, dpi=150).savefig(expdir + 'MCC-manhattan.svg')
-
-
-
+# def visualize(full_results, nonzero_results, expdir, reference):
+#     """
+#     Generate summary figures of EA-ML results, including a Manhattan plot of p-values and scatterplots and
+#     histograms of MCC scores
+#     """
+#     mcc_scatter(full_results, column='mean', dpi=150).savefig(expdir + f'meanMCC-scatter.png')
+#     mcc_hist(full_results, column='mean', dpi=150).savefig(expdir + f'meanMCC-hist.png')
+#     mcc_scatter(nonzero_results, column='MCC', dpi=150).savefig(expdir + 'meanMCC-scatter.nonzero.png')
+#     mcc_hist(nonzero_results, column='MCC', dpi=150).savefig(expdir + 'meanMCC-hist.nonzero.png')
+#     manhattan_plot(nonzero_results, reference, dpi=150).savefig(expdir + 'MCC-manhattan.svg')
 
