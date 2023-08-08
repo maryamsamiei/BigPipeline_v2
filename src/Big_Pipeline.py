2aaf7f5c4157e83cd9aa51fcd07d3d9a6b96ff7d
Maryam SN <69000702+maryamsamiei@users.noreply.github.com>
2023-08-08 10:58:03 -0500

    added option to exclude sex chr

--- src/Big_Pipeline.py
+++ src/Big_Pipeline.py
@@ -44,22 +44,26 @@ def parse_args():
     parser.add_argument('--cores', type=int, default=1, help='number of CPUs to use for multiprocessing')
     parser.add_argument('--pipeline', default='All', choices=('All', 'ML', 'Reactome', 'STRING','GoTerms', 'EAML', 'EPI', 'Wavelet'),help='which pipeline to be run')
     parser.add_argument('--JVMmemory', default='Xmx2g', help='memory argument for each Weka JVM')
+    parser.add_argument('--chrX', type=int,default=1, help='1 if there is sex chromosome in the VCF, 0 if there is no sex chromosome in the VCF')
 
     return parser.parse_args()
 
 
 def main(args):
-    if args.Ann=='ANNOVAR':
-        if args.ref=='hg19':
-            ref = pd.read_csv('./refs/refGene-lite_hg19.May2013.txt', delimiter='\t', header=0, index_col='gene')
-        elif args.ref=='hg38':
-            ref = pd.read_csv('./refs/refGene-lite_hg38.June2017.txt', delimiter='\t', header=0, index_col='gene')
-    elif args.Ann=='VEP':
-        if args.ref=='hg19':
-            ref = pd.read_csv('./refs/ENSEMBL-lite_GRCh37.v75.txt', delimiter='\t', header=0, index_col='gene')
-        elif args.ref=='hg38':
-            ref = pd.read_csv('./refs/ENSEMBL-lite_GRCh38.v94.txt', delimiter='\t', header=0, index_col='gene')
-        ref = ref[~ref.index.duplicated(keep='first')]    
+    if args.chrX==1:
+        if args.Ann=='ANNOVAR':
+            if args.ref=='hg19':
+                ref = pd.read_csv('./refs/refGene-lite_hg19.May2013.txt', delimiter='\t', header=0, index_col='gene')
+            elif args.ref=='hg38':
+                ref = pd.read_csv('./refs/refGene-lite_hg38.June2017.txt', delimiter='\t', header=0, index_col='gene')
+        elif args.Ann=='VEP':
+            if args.ref=='hg19':
+                ref = pd.read_csv('./refs/ENSEMBL-lite_GRCh37.v75.txt', delimiter='\t', header=0, index_col='gene')
+            elif args.ref=='hg38':
+                ref = pd.read_csv('./refs/ENSEMBL-lite_GRCh38.v94.txt', delimiter='\t', header=0, index_col='gene')
+    elif (args.chrX==0) & (args.ref=='hg38') & (args.Ann=='VEP'):
+        ref = pd.read_csv('./refs/ENSEMBL-lite_GRCh38.v94.noX.txt', delimiter='\t', header=0, index_col='gene')
+    ref = ref[~ref.index.duplicated(keep='first')]    
     text_file = open(args.savepath+'arguments.txt', 'w')
     text_file.write('You are running '+str(args.pipeline)+' pipeline for:'+'\n'+str(args.minaf)+'<Allele Frequency<'+str(args.maxaf)+ '\n'+ 
                     'Genome Reference:'+str(args.ref))
